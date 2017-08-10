278d46611692af4accb8a5e7395cd3d379c7c8ee
Shanker <shanker@yeah.net>
2017-08-10 08:04:15 +0800

    update

--- init_zsh.sh
+++ init_zsh.sh
@@ -57,10 +57,10 @@ function installAutojump(){
     cd ~/autojump
     ./install.py
 
-cat >>~/.zshrc<<EOF
-[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh
-autoload -U compinit && compinit -u
-EOF
+#cat >>~/.zshrc<<EOF
+#[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh
+#autoload -U compinit && compinit -u
+#EOF
 
 }
 
